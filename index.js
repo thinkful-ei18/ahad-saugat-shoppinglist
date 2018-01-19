48f8a90a2881795eae209e1e3380776b95d8af5b
Saugat Dhungel <saugat.dhungel@gmail.com>
2018-01-19 11:16:16 -0500

    shopping-list-refactored

---
+++ index.js
@@ -0,0 +1,35 @@
+'use strict';
+
+// enter items they need to purchase by entering text and hitting "Return" or clicking the "Add item" button
+// check and uncheck items on the list by clicking the "Check" button
+// permanently remove items from the list
+
+function createNewShoppingListElement(input) {
+  return `<li>
+            <span class="shopping-item">${input}</span>
+            <div class="shopping-item-controls">
+            <button class="shopping-item-toggle">
+              <span class="button-label">check</span>
+            </button>
+            <button class="shopping-item-delete">
+              <span class="button-label">delete</span>
+            </button>
+          </div>
+        </li>`;
+}
+
+$('form button[type="submit"]').on('click', event => {
+  event.preventDefault();
+  const input = $('form input[type="text"]').val();
+  $('form input[type="text"]').val('');
+  $('.shopping-list').append(createNewShoppingListElement(input));
+});
+
+$('.shopping-list').on('click','.shopping-item-toggle',event => {
+  $(event.currentTarget).closest('li').find('span:first').toggleClass('shopping-item__checked');
+  // listItem.toggleClass('.shopping-item__checked');
+});
+
+$('.shopping-list').on('click', '.shopping-item-delete', event => {
+  $(event.currentTarget).closest('li').remove();
+});
