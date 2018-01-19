57b8427c70409fefd7fa682f4a82c4d46ea1a476
Saugat Dhungel <saugat.dhungel@gmail.com>
2018-01-19 11:19:13 -0500

    shopping-list-refactored

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 'use strict';
 
-// enter items they need to purchase by entering text and hitting "Return" or clicking the "Add item" button
-// check and uncheck items on the list by clicking the "Check" button
-// permanently remove items from the list
-
 function createNewShoppingListElement(input) {
   return `<li>
             <span class="shopping-item">${input}</span>
